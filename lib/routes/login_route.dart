8440c5cd42bbfac5b19e0fa536345187c63ce0db
arunbabuvn <arunbabuvn@gmail.com>
2023-11-12 09:03:20 +0530

    safe area added

--- lib/routes/login_route.dart
+++ lib/routes/login_route.dart
@@ -15,85 +15,87 @@ class _LoginRouteState extends State<LoginRoute> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Container(
-          margin: const EdgeInsets.all(20),
-          child: Column(
-            children: [
-              const SizedBox(height: 30),
-              Image.asset(
-                "assets/images/logo.png",
-                width: 80,
-                height: 80,
-              ),
-              const SizedBox(height: 10),
-              Text(
-                "REAKT",
-                style: GoogleFonts.inter(
-                  fontSize: 32,
-                  fontWeight: FontWeight.w700,
+    return SafeArea(
+      child: Scaffold(
+        body: Center(
+          child: Container(
+            margin: const EdgeInsets.all(20),
+            child: Column(
+              children: [
+                const SizedBox(height: 30),
+                Image.asset(
+                  "assets/images/logo.png",
+                  width: 80,
+                  height: 80,
                 ),
-              ),
-              Text(
-                "Signin with your phone number",
-                style: GoogleFonts.inter(
-                  fontSize: 13,
-                  fontWeight: FontWeight.w400,
-                  color: const Color(0xFF06080A),
-                ),
-              ),
-              const SizedBox(height: 20),
-              Align(
-                alignment: Alignment.centerLeft,
-                child: Text(
-                  "Phone",
+                const SizedBox(height: 10),
+                Text(
+                  "REAKT",
                   style: GoogleFonts.inter(
-                    fontSize: 16,
-                    fontWeight: FontWeight.w500,
+                    fontSize: 32,
+                    fontWeight: FontWeight.w700,
+                  ),
+                ),
+                Text(
+                  "Signin with your phone number",
+                  style: GoogleFonts.inter(
+                    fontSize: 13,
+                    fontWeight: FontWeight.w400,
                     color: const Color(0xFF06080A),
                   ),
                 ),
-              ),
-              TextField(
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(20),
-                    borderSide: BorderSide.none,
-                  ),
-                  fillColor: const Color.fromRGBO(179, 179, 179, 0.1),
-                  filled: true,
-                  hintText: "+91 XXX XXXXXXX",
-                ),
-              ),
-              const Spacer(),
-              InkWell(
-                child: Container(
-                  height: 56,
-                  width: MediaQuery.sizeOf(context).width,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(20),
-                    color: const Color(0xFF06080A),
-                  ),
-                  child: Center(
-                    child: Text(
-                      "Continue",
-                      style: GoogleFonts.inter(
-                        fontSize: 16,
-                        fontWeight: FontWeight.w500,
-                        color: Colors.white,
-                      ),
+                const SizedBox(height: 20),
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    "Phone",
+                    style: GoogleFonts.inter(
+                      fontSize: 16,
+                      fontWeight: FontWeight.w500,
+                      color: const Color(0xFF06080A),
                     ),
                   ),
                 ),
-                onTap: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => const OtpRoute()),
-                  );
-                },
-              )
-            ],
+                TextField(
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(20),
+                      borderSide: BorderSide.none,
+                    ),
+                    fillColor: const Color.fromRGBO(179, 179, 179, 0.1),
+                    filled: true,
+                    hintText: "+91 XXX XXXXXXX",
+                  ),
+                ),
+                const Spacer(),
+                InkWell(
+                  child: Container(
+                    height: 56,
+                    width: MediaQuery.sizeOf(context).width,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      color: const Color(0xFF06080A),
+                    ),
+                    child: Center(
+                      child: Text(
+                        "Continue",
+                        style: GoogleFonts.inter(
+                          fontSize: 16,
+                          fontWeight: FontWeight.w500,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                  ),
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => const OtpRoute()),
+                    );
+                  },
+                )
+              ],
+            ),
           ),
         ),
       ),
