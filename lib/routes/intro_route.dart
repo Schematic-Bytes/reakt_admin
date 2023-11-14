236ecc081ad18213e9ae34d9403edb8c7864dbfd
arunbabuvn <arunbabuvn@gmail.com>
2023-11-14 20:39:29 +0530

    added intro, features route

---
+++ lib/routes/intro_route.dart
@@ -0,0 +1,77 @@
+import 'package:reakt_admin/routes/features_route.dart';
+// import 'package:reakt/widgets/permission.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class IntroRoute extends StatelessWidget {
+  const IntroRoute({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Container(
+          margin: const EdgeInsets.all(20),
+          child: Column(
+            children: [
+              const SizedBox(height: 10),
+              Image.asset("assets/images/intro-img.png"),
+              Text(
+                "Welcome to Reakt",
+                style: GoogleFonts.inter(
+                  fontSize: 36,
+                  fontWeight: FontWeight.w700,
+                  color: const Color(0xFF0A0606),
+                ),
+              ),
+              RichText(
+                text: TextSpan(
+                  text: "Safety is just a ",
+                  style: GoogleFonts.inter(
+                    fontSize: 17,
+                    fontWeight: FontWeight.w400,
+                    color: const Color(0xFF0A0606),
+                  ),
+                  children: [
+                    TextSpan(
+                      text: "Tap",
+                      style: GoogleFonts.inter(
+                          fontSize: 17, fontWeight: FontWeight.w800),
+                    ),
+                    const TextSpan(text: " away."),
+                  ],
+                ),
+              ),
+              const Spacer(),
+              InkWell(
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => const Features()),
+                  );
+                },
+                child: Container(
+                  padding: const EdgeInsets.all(16),
+                  width: MediaQuery.sizeOf(context).width,
+                  decoration: BoxDecoration(
+                    color: const Color(0xFF06080A),
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  child: Text(
+                    "Lets go !",
+                    style: GoogleFonts.inter(
+                      fontSize: 16,
+                      fontWeight: FontWeight.w500,
+                      color: const Color(0xFFFFFFFF),
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
